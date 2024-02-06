245da630f69ee561e913656ebb89dae12f795c43
Arjuna-1995 <aarjuna21@gmail.com>
2024-02-06 21:24:41 +0530

    second

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String args[])
     {
-        int a=6,b=8,c;
+        int a=6,b=8,c,d;
         c=a+b;
+        d=a-b;
         System.out.println(c);
+        System.out.println(d);
     }
 }
